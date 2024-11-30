b110a0169b0ca46b0b7bff46b01e87d33123ea8c
MuriloBarros304 <murilobarros304@gmail.com>
2024-11-30 17:10:45 -0300

    melhoria na lógica, thread de busca funcionando, função timer deletada, temporizador agora fica na search

--- codes/tello_control.py
+++ codes/tello_control.py
@@ -1,13 +1,15 @@
 import logging
-from tracking_base import tracking
-from detect_qr import process
 import time
+import threading
+from tracking_base import tracking, draw
+from detect_qr import process
 
 old_move = ''
 pace = ' 20'
-pace_moves = ['up', 'down', 'left', 'right', 'forward', 'back']
-start_time = time.time()  # Inicializa o tempo
+pace_moves = ['up', 'down', 'left', 'right', 'forward', 'back', 'cw', 'ccw']
 searching = False
+stop_searching = threading.Event()
+stop_receiving = threading.Event()
 
 logging.basicConfig(
     filename="codes/log.txt",
@@ -23,35 +25,29 @@ def log_command(command, response=None):
         command (str): Comando enviado ao drone.
         response (str, opcional): Resposta recebida do drone.
     '''
-    logging.info(f"{command}, {response}")
+    if response is None:
+        logging.info(command)
+    else:
+        logging.info(f"{command}, {response}")
 
-def timer():
+def search(tello):
     '''
-    Função que verifica se o timer de 5 segundos expirou.
-    Returns:
-        Booleano que indica se o timer expirou.
-    '''
-    global start_time
-    if time.time() - start_time >= 5:
-        start_time = time.time()  # Reinicia o timer
-        return True
-    return False
-
-def search(tello, frame):
-    '''
-    Função de busca que é chamada quando não há detecção de QR code após o timer expirar.
-    Executa uma rotação para procurar QR codes.
+    Procura por QR codes rotacionando o drone Tello em 20 graus para a direita e 40 graus para a esquerda.
     Args:
         tello: Objeto da classe TelloZune, que possui métodos para enviar comandos e obter estado.
-        frame: Frame de vídeo a ser processado para detecção de QR codes.
-    Returns:
-        frame: Frame processado após a execução da rotação
     '''
-    response = tello.send_cmd('ccw 20')  # Rotaciona 20 graus
-    time.sleep(0.01)                     # testar se resposta é exibida
-    print(f"Rotação: {response}")
-    log_command('ccw 20', response)
-    return frame
+    timer = time.time()
+    i = 0
+    commands = ['cw 20', 'ccw 40']
+    while not stop_searching.is_set() and not stop_receiving.is_set():
+        if time.time() - timer >= 5:                 # 5 segundos
+            response = tello.send_cmd(commands[i])   # Rotaciona 20 graus
+            time.sleep(0.1)                          # Testar se resposta é exibida
+            print(f"{commands[i]}, {response}")
+            log_command(commands[i], response)
+            timer = time.time()
+            i = (i + 1) % 2                          # Alterna entre 0 e 1
+        #print((time.time() - timer).__round__(2)) # Ver contagem regressiva
 
 def moves(tello, frame):
     '''
@@ -62,42 +58,48 @@ def moves(tello, frame):
     Returns:
         frame: Frame processado após a detecção e execução dos comandos.
     '''
-    global old_move, pace, pace_moves, searching, start_time
-    
-    frame, _, _, _, _, detections, text = process(frame)
+    global old_move, pace, pace_moves, searching
+    frame, x1, y1, x2, y2, detections, text = process(frame) # Agora process() retorna os valores de x1, y1, x2, y2, para ser chamada apenas uma vez
+    #frame, _, _, _, _, detections, text = process(frame)
 
     if detections > 0:
-        searching = False
-        start_time = time.time()
-    
+        if searching:
+            stop_searching.set() # Setar evento de parada
+            searching = False    # Parar busca
+
     elif detections == 0 and old_move != 'land':
-        if timer():
+        if not searching:
+            stop_searching.clear()                                         # Reseta o evento de parada
+            search_thread = threading.Thread(target=search, args=(tello,)) # Cria a thread de busca
+            search_thread.start()                                          # Inicia a thread
             searching = True
-            frame = search(tello, frame)
-        elif old_move == 'follow': # necessário para que o drone não continue a se movimentar sem detecção de follow
+
+        elif old_move == 'follow': # Necessário para que o drone não continue a se movimentar sem detecção de follow
             tello.send_rc_control(0, 0, 0, 0)
             log_command('rc 0 0 0 0')
 
-    if text == 'follow':
-        frame = tracking(tello, frame)
-        log_command('follow') # ver como vai ficar isso
-    
-    if detections == 1 and text == 'land':
+    if detections == 1 and text == 'follow':
+        frame = tracking(tello, frame, x1, y1, x2, y2, detections, text)
+        log_command(text)
+
+    elif detections == 1 and text == 'land':
         while float(tello.get_state_field('h')) >= 13:
             tello.send_rc_control(0, 0, -70, 0)
         tello.send_cmd(str(text))
         log_command(text)
-    
+
     elif detections == 1 and text == 'takeoff' and old_move != 'takeoff':
         response = tello.send_cmd_return(text)
-        print(f"{text}' '{response}")
-        log_command(text, response)
-    
+        time.sleep(1)
+        print(f"{text}, {response}")
+        log_command(f"{text}, {response}")
+
     elif detections == 1 and text in pace_moves:
         response = tello.send_cmd_return(f"{text}{pace}")
-        print(f"{text}{pace}' '{response}")
-        log_command(f"{text}{pace}", response)
+        frame = draw(frame, x1, y1, x2, y2, text)
+        print(f"{text}{pace}, {response}")
+        log_command(f"{text}{pace}, {response}")
 
     old_move = text
-    print(f"Old move: {old_move}")
+    #print(f"Old move: {old_move}")
     return frame
