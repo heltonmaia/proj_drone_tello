688ad80e748e5080b2b57cc35c16395758f47527
MuriloBarros304 <murilobarros304@gmail.com>
2024-12-16 13:15:56 -0300

    Readme atualizado, adicionada doctring em main.py

--- codes/main.py
+++ codes/main.py
@@ -1,7 +1,31 @@
+import cv2
+import threading
 from tello_zune import TelloZune
-import cv2, threading
 import modules.tello_control as tello_control
 from modules.utils import configureLogging
+"""
+Módulo principal para controle do drone Tello.
+
+Este arquivo inicializa a captura de vídeo, faz as configurações iniciais e direciona o fluxo de controle do drone.
+
+Funcionalidades principais:
+- Captura e exibição de vídeo em tempo real.
+- Processamento de QR codes para controle do drone.
+- Decolagem, pouso, busca e movimentação com base nos comandos recebidos.
+- Registro de todos os comandos enviados ao drone em um arquivo de log.
+
+Classes:
+- TelloZune: Interface principal para controle do drone.
+
+Módulos utilizados:
+- tello_control: Contém funções para movimentação e lógica de controle.
+- tracking_base: Funções para detectar e seguir QR codes.
+- qr_processing: Processamento de QR codes.
+- utils: Configuração de logging.
+
+Como executar:
+- Execute o arquivo main.py já conectado à rede Wi-Fi do drone Tello.
+"""
 
 # Inicialização
 cap = cv2.VideoCapture(0) # Captura de vídeo da webcam
@@ -19,15 +43,18 @@ try:
         ret, frame = cap.read() # Captura de vídeo da webcam
         #frame = tello.get_frame()
         #tello.calc_fps(frame)
+
         # Tratamento
         frame = cv2.resize(frame, (960, 720))
         frame = tello_control.moves(tello, frame)
+
         # Exibição
         cv2.imshow('QR Code', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             tello_control.stop_receiving.set() # Para encerrar a thread de busca
             break
 finally:
+    # Finalização
     cap.release()
     #tello.end_tello()
     cv2.destroyAllWindows()
