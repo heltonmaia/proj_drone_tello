088a7c58dd5ef2126fda13594112233ea709d61c
MuriloBarros304 <murilobarros304@gmail.com>
2025-03-23 09:37:59 -0300

    pequenas melhorias na interface

--- codes/interface.py
+++ codes/interface.py
@@ -4,16 +4,35 @@ import cv2
 import threading
 import modules.tello_control as tello_control
 from tello_zune import TelloZune
+import os, signal
+
+# Função para atualizar passo nos movimentos do drone
+def update_pace():
+    """Atualiza o valor de tello_control.pace"""
+    if "pace_input" in st.session_state:
+        tello_control.pace = st.session_state.pace_input
+
+# Função para atualizar valores
+def update_values():
+    """Atualiza os valores dos parâmetros dinamicamente"""
+    bat, height, temph, pres, time_elapsed = tello.get_info()
+    
+    with right_col:
+        st.session_state.battery_value.markdown(f"**{bat if bat is not None else 'N/A'}%**")
+        st.session_state.height_value.markdown(f"**{height if height is not None else 'N/A'}cm**")
+        st.session_state.temp_value.markdown(f"**{temph if temph is not None else 'N/A'}°C**")
+        st.session_state.pres_value.markdown(f"**{pres if pres is not None else 'N/A'}hPa**")
+        st.session_state.time_value.markdown(f"**{time_elapsed if time_elapsed is not None else 'N/A'}s**")
 
 # Inicialização do Drone
 if "tello" not in st.session_state:
     st.session_state.tello = TelloZune()
     st.session_state.command_log = []
-    st.session_state.params_initialized = False  # Controle de inicialização
+    st.session_state.params_initialized = False # Controle de inicialização
 
 tello = st.session_state.tello
 st.session_state.last_update = time.time()
-tello_control.enable_search = False
+tello_control.enable_search = False # Ativa a busca
 tello_control.stop_searching.clear()
 
 # Iniciar o drone apenas se ele ainda não estiver conectado
@@ -24,16 +43,16 @@ if not hasattr(tello, "receiverThread") or not tello.receiverThread.is_alive():
 
 # Configuração da Interface
 st.set_page_config(layout="wide")
-left_col, right_col = st.columns([3, 1])  # 3:1 proporção entre vídeo e parâmetros
+left_col, right_col = st.columns([3, 1]) # 3:1 vídeo e parâmetros
 frame_placeholder = left_col.empty()
-log_placeholder = st.empty()  # Log abaixo de todo o conteúdo
+log_placeholder = st.empty() # Log abaixo de todo o conteúdo
 
 # Inicialização dos elementos da coluna direita
 if not st.session_state.params_initialized:
     with right_col:
         # Container para bateria
         with st.container():
-            bat_col1, bat_col2 = st.columns([1, 3])
+            bat_col1, bat_col2 = st.columns([1, 3]) # 1:3 ícone e valor
             with bat_col1:
                 st.image("../images/battery_icon.png", width=40)
             with bat_col2:
@@ -93,11 +112,13 @@ with st.sidebar:
     if st.button("Encerrar Drone"):
         #cap.release()
         tello.end_tello()
-        st.session_state.tello.stop_receiving.set()
+        tello.stop_receiving.set()
         tello_control.stop_searching.set()
-        st.session_state.tello.moves_thread.join()
+        tello.moves_thread.join()
         del st.session_state.tello
         st.stop()
+        os.kill(os.getpid(), signal.SIGKILL) # Encerra o programa na marra
+    pace_input = st.text_input("Passo (cm): ", "50", key="pace_input", on_change=update_pace)
 
     st.write("---")
     st.subheader("Enviar Comando")
@@ -120,24 +141,12 @@ if not tello_control.searching:
     search_thread.start()
     tello_control.searching = True
 
-# Função para atualizar valores
-def update_values():
-    """Atualiza os valores dos parâmetros dinamicamente"""
-    bat, height, temph, pres, time_elapsed = tello.get_info()
-    
-    with right_col:
-        st.session_state.battery_value.markdown(f"**{bat if bat is not None else 'N/A'}%**")
-        st.session_state.height_value.markdown(f"**{height if height is not None else 'N/A'} cm**")
-        st.session_state.temp_value.markdown(f"**{temph if temph is not None else 'N/A'}°C**")
-        st.session_state.pres_value.markdown(f"**{pres if pres is not None else 'N/A'}**")
-        st.session_state.time_value.markdown(f"**{time_elapsed if time_elapsed is not None else 'N/A'} s**")
-
 # Loop principal
 while True:
     #ret, frame = cap.read()
     frame = tello.get_frame()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Conversão agora é feita aqui
     frame = tello_control.moves(tello, frame)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_placeholder.image(frame, channels="RGB")
 
     # Atualiza valores a cada 5 segundos
@@ -147,10 +156,10 @@ while True:
 
     # Atualiza logs
     if tello_control.log_messages:
-        st.session_state.command_log.extend(tello_control.log_messages)
-        tello_control.log_messages.clear()
+        st.session_state.command_log.extend(tello_control.log_messages) # Da lista para o log do Streamlit
+        tello_control.log_messages.clear() # Limpa a lista de mensagens
     
     log_placeholder.text("\n".join(st.session_state.command_log))
-    st.session_state.fps_value.markdown(f"**{tello.calc_fps()}**") # Atualiza FPS a cada frame
+    st.session_state.fps_value.markdown(f"**{tello.calc_fps()}fps**") # Atualiza FPS a cada frame
 
     #time.sleep(0.001)
