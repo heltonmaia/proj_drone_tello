8dcae13d7e45f22977260f55b1078fa08bd75ac0
MuriloBarros304 <murilobarros304@gmail.com>
2025-04-26 14:59:04 -0300

    chat_history removido, requirements e Readme atualizados

--- codes/interface.py
+++ codes/interface.py
@@ -1,23 +1,20 @@
 import streamlit as st
 import time
 import cv2
-import threading
-from datetime import datetime
 import modules.tello_control as tello_control
 import modules.chatbot as chatbot
 from tello_zune import TelloZune
 
 def initialize_session() -> None:
     """
-    Inicializa variáveis de sessão para drone, câmera e histórico.
+    Inicializa variáveis de sessão para drone, câmera e histórico do chatbot.
     """
     if "tello" not in st.session_state:
-        st.session_state.tello = TelloZune(simulate=False)
-        st.session_state.command_log = []
+        st.session_state.tello = TelloZune()
+        st.session_state.command_log = tello_control.log_messages
         st.session_state.params_initialized = False
-
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+        st.session_state.last_user = ""
+        st.session_state.last_ai = ""
 
     #if "cap" not in st.session_state:
     #    st.session_state.cap = cv2.VideoCapture(0) # webcam
@@ -37,9 +34,9 @@ def configure_interface() -> tuple:
     response_placeholder = st.empty()
     return left_col, right_col, frame_placeholder, text_input_placeholder, response_placeholder
 
-def render_parameters(right_col):
+def render_parameters(right_col) -> None:
     """
-    Renderiza os parâmetros do drone na coluna direita.
+    Parâmetros do drone na coluna direita.
     Args:
         right_col: Coluna da interface onde os parâmetros serão exibidos.
     """
@@ -47,16 +44,16 @@ def render_parameters(right_col):
         with right_col:
             icons = ['battery_icon.png', 'height_icon.png', 'temp_icon.png', 'pressure_icon.png', 'time_icon.png', 'fps_icon.png']
             keys = ['battery_value', 'height_value', 'temp_value', 'pres_value', 'time_value', 'fps_value']
-            for icon, key in zip(icons, keys):
+            for icon, key in zip(icons, keys): # Itera sobre os ícones e chaves
                 with st.container():
-                    c1, c2 = st.columns([1, 3])
+                    c1, c2 = st.columns([1, 3]) # Proporção 1:3
                     with c1:
                         st.image(f"../images/{icon}", width=40)
                     with c2:
                         st.session_state[key] = st.empty()
         st.session_state.params_initialized = True
 
-def render_sidebar():
+def render_sidebar() -> None:
     """
     Botões de controle e log na sidebar.
     """
@@ -73,8 +70,6 @@ def render_sidebar():
             st.session_state.tello.end_tello()
             st.session_state.tello.movesThread.stop()
             tello_control.stop_searching.set()
-            if hasattr(st.session_state.tello, 'moves_thread'):
-                st.session_state.tello.moves_thread.join()
             del st.session_state.tello
             st.stop()
 
@@ -87,7 +82,7 @@ def render_sidebar():
         for log in st.session_state.command_log:
             st.text(log)
 
-def update_pace():
+def update_pace() -> None:
     """Atualiza tello_control.pace."""
     tello_control.pace = st.session_state.pace_input
 
@@ -99,42 +94,29 @@ def render_text_input(text_input_placeholder) -> None:
     """
     with text_input_placeholder.container():
         user_input = st.text_input("Envie um comando para o drone:", key="user_input")
-        lock = threading.Lock()
         if st.button("Enviar") and user_input:
             #ret, frame = st.session_state.cap.read()
-            frame = st.session_state.tello.get_frame()
+            frame = st.session_state.tello.get_frame() # Por enquanto o frame é adquirido novamente
             current_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            natural_response, command = chatbot.run_ai(user_input, current_frame)
-            timestamp = datetime.now().strftime("%H:%M:%S")
-            entry = {
-                "user": user_input,
-                "ai": natural_response,
-                "command": command,
-                "timestamp": timestamp,
-                "status": "queued" if command else "error"
-            }
+            response, command = chatbot.run_ai(user_input, current_frame)
             if command and chatbot.validate_command(command):
                 tello_control.process_ai_command(st.session_state.tello, command)
-                st.session_state.command_log.append(command)
-                entry['execution'] = f"Comando enfileirado: {command}"
-            with lock:
-                st.session_state.chat_history.append(entry)
+                tello_control.log_messages.append(command)
+
+            # Atualiza última interação
+            st.session_state.last_user = user_input
+            st.session_state.last_ai = response
 
 def render_response(response_placeholder) -> None:
     """
-    Exibe histórico de chat.
+    Exibe interação do chatbot.
     Args:
         response_placeholder: Placeholder para exibir o histórico de chat.
     """
     with response_placeholder.container():
-        for entry in st.session_state.chat_history[-3:]: # Testar se exibe os últimos 3 comandos
-            st.markdown(f"**{entry['timestamp']} - Você:** {entry['user']}")
-            if entry['status'] == 'processing':
-                with st.spinner(entry['ai'], show_time=True):
-                    time.sleep(0.1)
-            else:
-                st.markdown(f"**{entry['timestamp']} - Drone:** {entry['ai']}")
-                st.write("\n")
+        if st.session_state.last_ai:
+            st.markdown(f"**Você:** {st.session_state.last_user}")
+            st.markdown(f"**Drone:** \n {st.session_state.last_ai}")
 
 def render_frame(frame_placeholder) -> None:
     """
